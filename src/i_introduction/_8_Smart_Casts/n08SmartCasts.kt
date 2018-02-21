9fa3f98f92e9c6b8dcca7f26133892bd195bbe2f
progsum <progsum@gmail.com>
2018-02-21 22:32:25 +0530

    Committing

--- src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
+++ src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
@@ -12,6 +12,7 @@ class Sum(val left: Expr, val right: Expr) : Expr()
 fun eval(e: Expr): Int =
         when (e) {
             is Num -> e.value
+            
             is Sum -> eval(e.left) + eval(e.right)
         }
 
