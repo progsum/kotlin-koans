49f766372ff30e2566afe033158aee3fcc1a6cb5
progsum <progsum@gmail.com>
2018-02-23 22:32:01 +0530

    Committing : check n28 n29

--- src/iii_conventions/n29OperatorsOverloading.kt
+++ src/iii_conventions/n29OperatorsOverloading.kt
@@ -1,5 +1,6 @@
 package iii_conventions
 
+import i_introduction._5_String_Templates.month
 import util.TODO
 import iii_conventions.TimeInterval.*
 
@@ -20,12 +21,33 @@ fun todoTask29(): Nothing = TODO(
     })
 
 fun task29_1(today: MyDate): MyDate {
-    todoTask29()
-//    return today + YEAR + WEEK
+    //todoTask29()
+    return today + YEAR + WEEK
+}
+
+private operator fun MyDate.plus(plusTime: TimeInterval): MyDate {
+    if(TimeInterval.YEAR.equals(plusTime)) return MyDate(this.year.inc(), month, dayOfMonth)
+    else if(TimeInterval.WEEK.equals(plusTime)) return MyDate(this.year.inc(), month, this.dayOfMonth+7)
+    else if(TimeInterval.DAY.equals(plusTime)) return MyDate(this.year.inc(), month, this.dayOfMonth+1)
+    return this;
+}
+
+private operator fun MyDate.plus(plusTime: MyDate): MyDate {
+    if(TimeInterval.YEAR.equals(plusTime)) return MyDate(this.year.inc(), month, dayOfMonth)
+    else if(TimeInterval.WEEK.equals(plusTime)) return MyDate(this.year.inc(), month, this.dayOfMonth+7)
+    else if(TimeInterval.DAY.equals(plusTime)) return MyDate(this.year.inc(), month, this.dayOfMonth+1)
+    return this;
 }
 
 fun task29_2(today: MyDate): MyDate {
-    todoTask29()
-//    return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    //todoTask29()
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
+}
+
+private operator fun TimeInterval.times(i: Int): MyDate {
+    if(TimeInterval.YEAR.equals(i)) return MyDate(ordinal*i, 0, 0)
+    else if(TimeInterval.WEEK.equals(i)) return MyDate(0, 0, ordinal*i*7)
+    else if(TimeInterval.DAY.equals(i)) return MyDate(0,0,ordinal*i)
+    return MyDate(0,0,0)
 }
 
